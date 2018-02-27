e3608b8cd8a1c1200636de4a50fa69d555398662
Andres Wong <wongo13a@gmail.com>
2018-02-27 00:51:35 -0800

    Update Program

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -7,8 +7,6 @@ namespace Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 3!");
             
             Console.ReadKey();        
         }
